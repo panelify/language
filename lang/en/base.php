20699026654e2eaca23918306c4e25ae098e9ccb
majidmohammadian <majeedmohammadian@gmail.com>
2025-08-13 21:32:44 +0330

    added language files

---
+++ lang/en/base.php
@@ -0,0 +1,64 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Base Panelify Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during Language for
+    | various messages that we need to display to the user.
+    |
+    */
+
+    "name" => "Languages",
+
+    "list" => [
+        "filters" => [
+            "name" => [
+                "title" => "Name",
+                "placeholder" => "Enter name",
+            ],
+        ],
+        "columns" => [
+            "flag" => "Flag",
+            "locale" => "Locale",
+            "direction" => "Direction",
+            "calendar" => "Calendar Type",
+        ],
+    ],
+
+    "form" => [
+        "create" => [
+            "title" => "Create Language",
+        ],
+        "edit" => [
+            "title" => "Edit Language",
+        ],
+        "fields" => [
+            "name" => [
+                "title" => "Name",
+                "placeholder" => "Enter name",
+            ],
+            "locale" => [
+                "title" => "Locale",
+                "placeholder" => "Enter locale",
+            ],
+            "direction" => [
+                "title" => "Direction",
+                "rtl" => "Right to Left (rtl)",
+                "ltr" => "Left to Right (ltr)",
+            ],
+            "calendar" => [
+                "title" => "Calendar",
+                "placeholder" => "Select calendar",
+            ],
+            "flag" => [
+                "title" => "Flag",
+                "placeholder" => "Select flag",
+            ],
+        ],
+    ],
+
+];
---
+++ lang/fa/base.php
@@ -0,0 +1,64 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Base Panelify Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during Language for
+    | various messages that we need to display to the user.
+    |
+    */
+
+    "name" => "زبان ها",
+
+    "list" => [
+        "filters" => [
+            "name" => [
+                "title" => "نام",
+                "placeholder" => "نام را وارد کنید",
+            ],
+        ],
+        "columns" => [
+            "flag" => "پرچم",
+            "locale" => "کلید زبان",
+            "direction" => "جهت",
+            "calendar" => "نوع تقویم",
+        ],
+    ],
+
+    "form" => [
+        "create" => [
+            "title" => "ایجاد زبان",
+        ],
+        "edit" => [
+            "title" => "ویرایش زبان",
+        ],
+        "fields" => [
+            "name" => [
+                "title" => "نام",
+                "placeholder" => "نام زبان را وارد کنید.",
+            ],
+            "locale" => [
+                "title" => "کلید زبان",
+                "placeholder" => "کلید زبان را وارد کنید",
+            ],
+            "direction" => [
+                "title" => "جهت",
+                "rtl" => "راست به چپ (rtl)",
+                "ltr" => "چپ به راست (ltr)",
+            ],
+            "calendar" => [
+                "title" => "تقویم",
+                "placeholder" => "تقویم را انتخاب کنید",
+            ],
+            "flag" => [
+                "title" => "پرچم",
+                "placeholder" => "پرچم را انتخاب کنید",
+            ],
+        ],
+    ],
+
+];
